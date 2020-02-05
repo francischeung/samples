b4e5adbf0a50bc0f215923f6c5640b4e67d4f665
Francis Cheung <fcheung@microsoft.com>
2020-02-05 15:29:38 -0800

    Updated onboarding script to read  creds from config file and translate user name/email to objectId

--- onboard_user.sh
+++ onboard_user.sh
@@ -1,34 +1,47 @@
-# Adds user (member-id) to Azure Active Directory group
-
 #!/bin/bash
+
+###
+### This is sample code and is not intended to be used in production
+###
+
+# Adds user to Azure Active Directory group
 
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -m memberid -g groupname"
-   echo -e "\t-m user's memberid"
+   echo "Usage: $0 -u userid -g groupname"
+   echo -e "\t-u user's id or email address"
    echo -e "\t-g name of AAD group"
    
    exit 1 # Exit script after printing help
 }
 
-while getopts "m:g:" opt
+while getopts "u:g:" opt
 do
    case "$opt" in
-      m ) memberid="$OPTARG" ;;
+      m ) userid="$OPTARG" ;;
       g ) groupname="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$memberid" ] || [ -z "$groupname" ]
+if [ -z "$userid" ] || [ -z "$groupname" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
 # Begin script in case all parameters are correct
-#echo "$memberid"
+#echo "$userid"
 #echo "$groupname"
-az ad group member add --group $groupname --member-id $memberid
+
+. serviceprincipalcreds.config
+
+#echo "using the following credentials to login:" "$clientid" "$clientsecret" "$tenantid"
+az login --service-principal -u "$clientid" -p "$clientsecret" --tenant "$tenantid"
+
+userObjId=$(az ad user show --id "$userid" -o json --query objectId)
+#echo "$userObjId"
+
+az ad group member add --group $groupname --member-id $userObjId
