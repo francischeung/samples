c3112d1865232d83bebee41c397e1902b0571662
Francis Cheung <fcheung@microsoft.com>
2020-04-15 13:17:01 -0700

    Cleaned up folder struction and removed unused package reference.

---
+++ function-sas-token/FunctionSasToken/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(FunctionSasToken.Startup))]
+
+namespace FunctionSasToken
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
