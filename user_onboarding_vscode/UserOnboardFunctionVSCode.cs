b2db82558f5bebd71e29882d4f10f33f046d40a0
Francis Cheung <fcheung@microsoft.com>
2020-04-20 14:34:20 -0700

    Updated User Onboarding code to use Micrsoft Graph API

--- user_onboarding_vscode/UserOnboardFunctionVSCode.cs
+++ user_onboarding_vscode/UserOnboardFunctionVSCode.cs
@@ -10,6 +10,10 @@ using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 using Newtonsoft.Json;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using System.Collections.Generic;
 
 namespace UserOnboardFunctionVSCode
 {
@@ -26,27 +30,46 @@ namespace UserOnboardFunctionVSCode
             log.LogDebug($"requestBody: {requestBody}");
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
+            var aADGroupName = "groupname";
+            var userName = "username@domain.com";
+
             //Key Vault secrets pulled using App Settings to reference Key Vault
             //https://docs.microsoft.com/en-us/azure/app-service/app-service-key-vault-references
             var clientId = Environment.GetEnvironmentVariable("ClientId", EnvironmentVariableTarget.Process);
             var clientSecret = Environment.GetEnvironmentVariable("ClientSecret", EnvironmentVariableTarget.Process);
             var tenantId = Environment.GetEnvironmentVariable("TenantId", EnvironmentVariableTarget.Process);
 
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
-            var authenticated = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials);
+            var confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithTenantId(tenantId)
+                .WithClientSecret(clientSecret)
+                .Build();
 
-            var user = await authenticated.ActiveDirectoryUsers.GetByNameAsync(data.username);
-            log.LogDebug($"user: {user?.Name}");
+            var authProvider = new ClientCredentialProvider(confidentialClientApplication);
+            var graphClient = new GraphServiceClient(authProvider);
 
-            var group = await authenticated.ActiveDirectoryGroups.GetByNameAsync(data.groupname);
-            log.LogDebug($"group: {group?.Name}");
+            var options = new List<QueryOption>
+                {
+                    new QueryOption("$filter",
+                      $"displayName eq '{aADGroupName}'")
+                };
 
-            await group.Update()
-                .WithMember(user)
-                .ApplyAsync();
+            var page = await graphClient.Groups
+                .Request(options)
+                .GetAsync();
+
+            if (page == null || page.Count != 1)
+                throw new ArgumentException($"Can not find matching group: {aADGroupName} in AAD");
+
+            var selectedGroup = page[0];
+
+            log.LogInformation($"Found group in AAD:{selectedGroup.DisplayName} Id:{selectedGroup.Id}");
+
+            var user = await graphClient.Users[userName]
+                .Request()
+                .GetAsync();
+                
+            await graphClient.Groups[selectedGroup.Id].Members.References.Request().AddAsync(user);
 
             return new OkResult();
         }
