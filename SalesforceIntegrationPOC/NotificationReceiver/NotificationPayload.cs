8808a708ce80f3dc47ecdce6058285ff3eef04c5
Francis Cheung <salsero888@gmail.com>
2022-03-07 16:43:55 -0800

    Fleshing out NotificationReceiver function

---
+++ SalesforceIntegrationPOC/NotificationReceiver/NotificationPayload.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace NotificationReceiver
+{
+    internal class NotificationPayload
+    {
+        public string SessionId { get; set; }
+        public IList<IDictionary<string, string>> Notifications { get; set; }
+    }
+}
--- SalesforceIntegrationPOC/NotificationReceiver/NotificationReceiverFunction.cs
+++ SalesforceIntegrationPOC/NotificationReceiver/NotificationReceiverFunction.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace NotificationReceiver
 {
@@ -14,6 +17,10 @@ namespace NotificationReceiver
         [FunctionName("NotificationReceiverFunction")]
         public async Task Run([EventHubTrigger("notifications", Connection = "EHConnection")] EventData[] events, ILogger log)
         {
+            var httpClient = new HttpClient();
+            var objectApiUri = System.Environment.GetEnvironmentVariable("ObjectApiUri", EnvironmentVariableTarget.Process);
+            httpClient.BaseAddress = new Uri(objectApiUri);
+
             var exceptions = new List<Exception>();
 
             foreach (EventData eventData in events)
@@ -21,6 +28,27 @@ namespace NotificationReceiver
                 try
                 {
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+
+                    var payload = JsonConvert.DeserializeObject<NotificationPayload>(messageBody);
+                    if (payload != null && payload.Notifications != null)
+                    {
+                        foreach (var notification in payload.Notifications)
+                        {
+                            if (notification["_NotificationType"] == "Apttus_Config2__LineItem__c")
+                            {
+                                var lineItemAPIObject = new LineItemAPIObject()
+                                {
+                                    Name = notification["Name"],
+                                    ExternalId = notification["Id"]
+                                };
+
+                                var httpContent = new StringContent(JsonConvert.SerializeObject(lineItemAPIObject));
+                                httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                                var response = await httpClient.PostAsync("", httpContent);
+                            }
+                        }
+                    }
 
                     // Replace these two lines with your processing logic.
                     log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
