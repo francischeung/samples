e5b142231f047ffc0206dcfb1783b51d22066e5a
Francis Cheung <salsero888@gmail.com>
2023-08-17 17:50:58 -0700

    Delete SalesforceIntegrationPOC directory

--- SalesforceIntegrationPOC/SalesforceWebhook/SalesforceWebhook/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SalesforceWebhook.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SalesforceIntegrationPOC/SalesforceWebhook/SalesforceWebhook/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SalesforceWebhook.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
