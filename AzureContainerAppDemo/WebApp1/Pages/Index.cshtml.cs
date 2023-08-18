d91272d3cf7d9afc9112d338d29399322c761221
Francis Cheung <salsero888@gmail.com>
2023-08-17 17:46:18 -0700

    adding ACA demo

---
+++ AzureContainerAppDemo/WebApp1/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public string WeatherForecast { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+            WeatherForecast = string.Empty;
+        }
+
+        public async Task OnGet()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(_config["WebAPIBaseAddress"]);
+            this.WeatherForecast = await httpClient.GetStringAsync("/weatherforecast");
+        }
+    }
+}
---
+++ AzureContainerAppDemo/WebAppDapr1/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public IEnumerable<WeatherForecast>? WeatherForecast { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public async Task OnGet()
+        {
+            var daprClient = new DaprClientBuilder().Build();
+            var result = daprClient.CreateInvokeMethodRequest(HttpMethod.Get, "webapiapp-dapr", "WeatherForecast");
+            this.WeatherForecast = await daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(result);
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
